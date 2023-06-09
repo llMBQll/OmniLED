09b8875df05eb05a82f8809bfd985c3d5a8a2fbc
llMBQll <50263781+llMBQll@users.noreply.github.com>
2023-06-09 22:27:25 +0200

    Remove the need to register apps at all

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,63 +1,29 @@
-use std::collections::hash_map::DefaultHasher;
-use std::collections::HashMap;
-use std::hash::{Hash, Hasher};
-use std::sync::{Arc, Mutex};
 use mlua::Lua;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::time::{SystemTime, UNIX_EPOCH};
-use warp::{Filter};
+use warp::Filter;
+
 use crate::server::update_handler::load_update_handler;
 
 pub struct Server {}
 
 impl Server {
     pub fn new(lua: &Lua) {
-        let state = Arc::new(Mutex::new(State::new()));
-
-        let register = warp::path!("register")
-            .and(warp::body::json())
-            .map({
-                let state = Arc::clone(&state);
-                move |data: RegisterData| {
-                    let token = state.lock().unwrap().add(data.name);
-
-                    let reply = warp::reply::json(&RegisterReply { token });
-                    warp::reply::with_status(reply, warp::http::StatusCode::OK)
-                }
-            });
-
         let handler = load_update_handler(lua);
-        let update = warp::path!("update" / u64)
+        let update = warp::path!("update")
             .and(warp::body::json())
-            .map({
-                let state = Arc::clone(&state);
-                let handler = Arc::clone(&handler);
-                move |token: u64, update_data: UpdateData| {
-                    match state.lock().unwrap().get(token) {
-                        Some(name) => {
-                            handler.lock().unwrap().push((name.clone(), update_data.fields));
+            .map(move |update_data: UpdateData| {
+                handler.lock().unwrap().push((update_data.name, update_data.fields));
 
-                            let reply = warp::reply::json(&UpdateReply { error: None });
-                            warp::reply::with_status(reply, warp::http::StatusCode::OK)
-                        }
-                        None => {
-                            let reply = warp::reply::json(&UpdateReply {
-                                error: Some(String::from("Application not registered."))
-                            });
-                            warp::reply::with_status(reply, warp::http::StatusCode::BAD_REQUEST)
-                        }
-                    }
-                }
+                let reply = warp::reply::json(&UpdateReply { error: None });
+                warp::reply::with_status(reply, warp::http::StatusCode::OK)
             });
-
-        let paths = warp::post()
-            .and(register)
-            .or(update);
 
         // Try to bind server to first available port and start accepting requests
         let mut port: u16 = 6969;
         let (address, server) = loop {
-            match warp::serve(paths.clone()).try_bind_ephemeral(([127, 0, 0, 1], port)) {
+            match warp::serve(update.clone()).try_bind_ephemeral(([127, 0, 0, 1], port)) {
                 Ok((address, server)) => {
                     break (address.to_string(), server);
                 }
@@ -92,19 +58,8 @@ impl Server {
 }
 
 #[derive(Deserialize)]
-#[serde(deny_unknown_fields)]
-struct RegisterData {
-    name: String,
-}
-
-#[derive(Serialize)]
-struct RegisterReply {
-    token: u64,
-}
-
-#[derive(Deserialize)]
 struct UpdateData {
-    #[serde(flatten)]
+    name: String,
     fields: HashMap<String, serde_json::Value>,
 }
 
@@ -112,30 +67,4 @@ struct UpdateData {
 struct UpdateReply {
     #[serde(skip_serializing_if = "Option::is_none")]
     error: Option<String>,
-}
-
-struct State {
-    names: HashMap<u64, String>,
-}
-
-impl State {
-    pub fn new() -> Self {
-        Self {
-            names: HashMap::new(),
-        }
-    }
-
-    pub fn add(&mut self, name: String) -> u64 {
-        let mut s = DefaultHasher::new();
-        name.hash(&mut s);
-        let token = s.finish();
-
-        self.names.insert(token, name);
-
-        token
-    }
-
-    pub fn get(&self, token: u64) -> Option<&String> {
-        self.names.get(&token)
-    }
 }
