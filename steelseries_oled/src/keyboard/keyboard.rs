6903be773046057ea642dd51cc2c2ade67935245
llMBQll <50263781+llMBQll@users.noreply.github.com>
2023-11-07 23:33:14 +0100

    Use poll based mechanism for keyboard events
    This approach is taken so a system level handler isn't held for as long as before, which caused the keyboard to sometimes feel unresponsive

--- steelseries_oled/src/keyboard/keyboard.rs
+++ steelseries_oled/src/keyboard/keyboard.rs
@@ -1,30 +1,37 @@
-use log::error;
-use rdev::{listen, EventType, Key};
+use device_query::{DeviceQuery, DeviceState, Keycode};
+use std::time::Duration;
 
 use crate::events::event_queue::{Event, EventQueue};
 
-pub fn process_events() {
-    if let Err(err) = listen(|event| {
-        let event_queue = EventQueue::instance();
-        match event.event_type {
-            EventType::KeyPress(key) => {
-                let mut event_queue = event_queue.lock().unwrap();
-                event_queue.push(Event::Keyboard(KeyboardEvent {
-                    key,
-                    event_type: KeyboardEventEventType::Press,
-                }));
+pub async fn process_events() {
+    let device_state = DeviceState::new();
+    let event_queue = EventQueue::instance();
+    let mut previous_state: Vec<Keycode> = Vec::new();
+
+    loop {
+        let keys = device_state.get_keys();
+        {
+            let mut guard = event_queue.lock().unwrap();
+            for key in keys.iter() {
+                if !previous_state.contains(&key) {
+                    guard.push(Event::Keyboard(KeyboardEvent {
+                        key: key.clone(),
+                        event_type: KeyboardEventEventType::Press,
+                    }));
+                }
             }
-            EventType::KeyRelease(key) => {
-                let mut event_queue = event_queue.lock().unwrap();
-                event_queue.push(Event::Keyboard(KeyboardEvent {
-                    key,
-                    event_type: KeyboardEventEventType::Release,
-                }));
+            for key in previous_state {
+                if !keys.contains(&key) {
+                    guard.push(Event::Keyboard(KeyboardEvent {
+                        key,
+                        event_type: KeyboardEventEventType::Release,
+                    }));
+                }
             }
-            _ => {}
         }
-    }) {
-        error!("Keyboard event handler failed: {:?}", err);
+        previous_state = keys;
+
+        tokio::time::sleep(Duration::from_millis(10)).await;
     }
 }
 
@@ -34,6 +41,6 @@ pub enum KeyboardEventEventType {
 }
 
 pub struct KeyboardEvent {
-    pub key: Key,
+    pub key: Keycode,
     pub event_type: KeyboardEventEventType,
 }
