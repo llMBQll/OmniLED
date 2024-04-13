bd9c3294f203e2db900a91637c97bfed35593793
llMBQll <50263781+llMBQll@users.noreply.github.com>
2024-04-13 18:54:18 +0200

    Send current weather image

--- oled-applications/weather/src/main.rs
+++ oled-applications/weather/src/main.rs
@@ -1,9 +1,10 @@
-use chrono::Timelike;
 use clap::Parser;
-use oled_api::types::{Image, Table};
+use oled_api::types::Table;
 use oled_api::Api;
 use std::{collections::HashMap, thread, time};
 use ureq::Agent;
+
+mod weather_api;
 
 const NAME: &str = "WEATHER";
 
@@ -21,7 +22,7 @@ fn main() {
     let agent = Agent::new();
 
     loop {
-        let weather = get_weather(&agent, &coordinates, &name);
+        let weather = weather_api::get_weather(&agent, &coordinates, &name);
         api.update(weather.into());
 
         thread::sleep(time::Duration::from_secs(15 * 60));
@@ -29,24 +30,15 @@ fn main() {
 }
 
 fn load_and_send_images(api: &Api) {
-    // TODO load the rest of images and attribute the creator
-    // images downloaded from https://www.flaticon.com/packs/weather-160
+    // TODO Attribute the creator, images downloaded from https://www.flaticon.com/packs/weather-160
 
-    const CLOUD: &[u8] = include_bytes!("../assets/cloud.png");
-
-    let mut cloud = image::load_from_memory_with_format(CLOUD, image::ImageFormat::Png).unwrap();
-    cloud.invert();
-    let grayscale = cloud.into_luma8();
-
-    let image = Image {
-        width: grayscale.width() as i64,
-        height: grayscale.height() as i64,
-        data: grayscale.into_raw(),
-    };
+    let images = weather_api::load_images();
 
     let mut table = Table::default();
-    table.items.insert("Cloudy".into(), image.into());
-    api.update(table);
+    for (name, image) in images {
+        table.items.insert(name.into(), image.into());
+    }
+    api.update(table)
 }
 
 fn get_coordinates_from_name(name: &Name) -> Coordinates {
@@ -86,51 +78,21 @@ fn get_coordinates_from_name(name: &Name) -> Coordinates {
         .expect("Couldn't find coordinates for the given query")
 }
 
-fn get_weather(agent: &Agent, coordinates: &Coordinates, city: &String) -> Data {
-    const OPEN_METEO_BASE: &str = "https://api.open-meteo.com/v1/forecast";
-
-    let res = agent
-        .get(OPEN_METEO_BASE)
-        .query("current_weather", "true")
-        .query("latitude", &coordinates.latitude.to_string())
-        .query("longitude", &coordinates.longitude.to_string())
-        .call()
-        .unwrap();
-    let result: WeatherResult = res.into_json().unwrap();
-
-    let time = format!("{}:00-00:00", result.current_weather.time);
-    let time: chrono::DateTime<chrono::Local> = chrono::DateTime::parse_from_rfc3339(time.as_str())
-        .unwrap()
-        .into();
-
-    Data {
-        latitude: result.latitude,
-        longitude: result.longitude,
-        temperature: result.current_weather.temperature,
-        wind_speed: result.current_weather.windspeed,
-        wind_direction: result.current_weather.winddirection,
-        weather_code: result.current_weather.weathercode,
-        is_day: result.current_weather.is_day != 0,
-        update_hour: time.hour(),
-        update_minute: time.minute(),
-        city: city.clone(),
-    }
-}
-
-struct Data {
+struct WeatherData {
     latitude: f64,
     longitude: f64,
     temperature: f64,
     wind_speed: f64,
     wind_direction: u32,
-    weather_code: u32,
     is_day: bool,
+    weather_description: String,
+    image_key: &'static str,
     update_hour: u32,
     update_minute: u32,
     city: String,
 }
 
-impl Into<Table> for Data {
+impl Into<Table> for WeatherData {
     fn into(self) -> Table {
         let mut table = Table::default();
 
@@ -151,7 +113,11 @@ impl Into<Table> for Data {
             .insert("WindDirection".to_string(), self.wind_direction.into());
         table
             .items
-            .insert("WeatherCode".to_string(), self.weather_code.into());
+            .insert("ImageKey".to_string(), self.image_key.into());
+        table.items.insert(
+            "WeatherDescription".to_string(),
+            self.weather_description.into(),
+        );
         table.items.insert("IsDay".to_string(), self.is_day.into());
         table
             .items
@@ -163,26 +129,6 @@ impl Into<Table> for Data {
 
         table
     }
-}
-
-#[derive(serde::Deserialize)]
-struct CurrentWeatherResult {
-    temperature: f64,
-    windspeed: f64,
-    winddirection: u32,
-    weathercode: u32,
-    is_day: u32,
-    time: String,
-}
-
-#[derive(serde::Deserialize)]
-struct WeatherResult {
-    latitude: f64,
-    longitude: f64,
-    current_weather: CurrentWeatherResult,
-
-    #[serde(flatten)]
-    pub _other: HashMap<String, serde_json::Value>,
 }
 
 #[derive(clap::Args)]
