b9b966bba4b6b78818f95c6bcf1387d3e83d524a
llMBQll <m.balabanow@gmail.com>
2024-12-19 14:21:46 +0100

    Add "Steelseries Apex Pro" keyboard configuration

--- config/devices.lua
+++ config/devices.lua
@@ -23,6 +23,31 @@ usb_device {
     memory_representation = 'BitPerPixel',
 }
 
+usb_device {
+    name = 'Steelseries Apex Pro',
+    screen_size = {
+        width = 128,
+        height = 40,
+    },
+    usb_settings = {
+        vendor_id = '0x1038',
+        product_id = '0x1610',
+        interface = '0x01',
+        alternate_setting = '0x00',
+        request_type = '0x21',
+        request = '0x09',
+        value = '0x0300',
+        index = '0x01',
+    },
+    transform = function(buffer)
+        local bytes = buffer:bytes()
+        table.insert(bytes, 1, 0x61)
+        table.insert(bytes, 0x00)
+        return bytes
+    end,
+    memory_representation = 'BitPerPixel',
+}
+
 emulator {
     name = 'Emulator',
     screen_size = {
