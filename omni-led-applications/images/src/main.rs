f55910b28057dd3041f7ebdfe84a3d305932a8e0
llMBQll <m.balabanow@gmail.com>
2024-12-27 14:16:04 +0100

    Create images application frame

---
+++ omni-led-applications/images/src/main.rs
@@ -0,0 +1,35 @@
+/*
+ * OmniLED is a software for displaying data on various OLED devices.
+ * Copyright (C) 2024  Michał Bałabanow <m.balabanow@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use clap::Parser;
+use omni_led_api::plugin::Plugin;
+
+const NAME: &str = "IMAGES";
+
+#[tokio::main]
+async fn main() {
+    let options = Options::parse();
+    let _plugin = Plugin::new(NAME, &options.address).await.unwrap();
+}
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Options {
+    #[clap(short, long)]
+    address: String,
+}
