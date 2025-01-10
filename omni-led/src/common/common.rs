ed27ac3c123c506261a14cbea2fa8b839079a270
llMBQll <m.balabanow@gmail.com>
2025-01-10 15:34:51 +0100

    Cleanup lua globals table creation

--- omni-led/src/common/common.rs
+++ omni-led/src/common/common.rs
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-use mlua::{Lua, ObjectLike, Table, Value};
+use mlua::{chunk, ErrorContext, Lua, ObjectLike, Table, Value};
 use omni_led_api::types::field::Field as FieldEntry;
 use omni_led_api::types::Field;
 
@@ -31,55 +31,90 @@ macro_rules! create_table {
 
 #[macro_export]
 macro_rules! create_table_with_defaults {
-    ($lua:ident, $values:tt) => {
-        {
-            let dump_fn = $lua.create_function(|_, value: mlua::Value| {
-                let string = format!("{:#?}", value);
-                Ok(string)
-            }).unwrap();
+    ($lua:ident, $values:tt) => {{
+        $lua.load(chunk! {
+            new_table = $values
+            new_table.assert = assert
+            new_table.coroutine = internal.table_copy(coroutine)
+            new_table.dump = internal.dump
+            new_table.getmetatable = getmetatable
+            new_table.ipairs = ipairs
+            new_table.math = internal.table_copy(math)
+            new_table.math.round = internal.round
+            new_table.next = next
+            new_table.os = {
+                clock = os.clock,
+                date = os.date,
+                difftime = os.difftime,
+                getenv = os.getenv,
+                time = os.time
+            }
+            new_table.pairs = pairs
+            new_table.pcall = pcall
+            new_table.print = print
+            new_table.string = internal.table_copy(string)
+            new_table.table = internal.table_copy(table)
+            new_table.tonumber = tonumber
+            new_table.tostring = tostring
+            new_table.type = type
+            new_table.utf8 = internal.table_copy(utf8)
+            return new_table
+        })
+        .eval::<mlua::Table>()
+        .unwrap()
+    }};
+}
 
-            let round_fn = $lua.create_function(|_, value: f64| {
-                let value = value.round() as i64;
-                Ok(value)
-            }).unwrap();
+pub fn load_internal_functions(lua: &Lua) {
+    let dump = lua
+        .create_function(|_, value: Value| {
+            let string = format!("{:#?}", value);
+            Ok(string)
+        })
+        .unwrap();
 
-            $lua.load(chunk! {
-                new_table = $values
-                new_table["dump"] = $dump_fn
-                new_table["ipairs"] = ipairs
-                new_table["next"] = next
-                new_table["pairs"] = pairs
-                new_table["pcall"] = pcall
-                new_table["print"] = print
-                new_table["tonumber"] = tonumber
-                new_table["tostring"] = tostring
-                new_table["type"] = type
-                new_table["coroutine"] = { close = coroutine.close, create = coroutine.create, isyieldable = coroutine.isyieldable, resume = coroutine.resume, running = coroutine.running, status = coroutine.status, wrap = coroutine.wrap, yield = coroutine.yield }
-                new_table["math"] = { abs = math.abs, acos = math.acos, asin = math.asin, atan = math.atan, atan2 = math.atan2, ceil = math.ceil, cos = math.cos, cosh = math.cosh, deg = math.deg, exp = math.exp, floor = math.floor, fmod = math.fmod, frexp = math.frexp, huge = math.huge, ldexp = math.ldexp, log = math.log, log10 = math.log10, max = math.max, maxinteger = math.maxinteger, min = math.min, mininteger = math.mininteger, modf = math.modf, pi = math.pi, pow = math.pow, rad = math.rad, random = math.random, randomseed = math.randomseed, round = $round_fn, sin = math.sin, sinh = math.sinh, sqrt = math.sqrt, tan = math.tan, tanh = math.tanh, tointeger = math.tointeger, type = math.type, ult = math.ult }
-                new_table["os"] = { clock = os.clock, date = os.date, difftime = os.difftime, getenv = os.getenv, time = os.time }
-                new_table["string"] = { byte = string.byte, char = string.char, dump = string.dump, find = string.find, format = string.format, gmatch = string.gmatch, gsub = string.gsub, len = string.len, lower = string.lower, match = string.match, pack = string.pack, packsize = string.packsize, rep = string.rep, reverse = string.reverse, sub = string.sub, unpack = string.unpack, upper = string.upper }
-                new_table["table"] = { concat = table.concat, insert = table.insert, move = table.move, pack = table.pack, remove = table.remove, sort = table.sort, unpack = table.unpack }
-                return new_table
-            }).eval::<mlua::Table>().unwrap()
-        }
-    };
+    let round = lua
+        .create_function(|_, value: f64| {
+            let value = value.round() as i64;
+            Ok(value)
+        })
+        .unwrap();
+
+    let table_copy = lua
+        .create_function(|lua, table: Table| -> mlua::Result<Table> {
+            let new_table = lua.create_table().unwrap();
+            for pair in table.pairs::<String, Value>() {
+                let (key, value) = pair?;
+                new_table.set(key, value)?;
+            }
+            Ok(new_table)
+        })
+        .unwrap();
+
+    lua.globals()
+        .set(
+            "internal",
+            create_table!(lua, {
+                dump = $dump,
+                round = $round,
+                table_copy = $table_copy
+            }),
+        )
+        .unwrap();
 }
 
 pub fn exec_file(lua: &Lua, name: &str, env: Table) -> mlua::Result<()> {
     let (func, err): (Value, Value) = lua.globals().call_function("loadfile", (name, "t", env))?;
 
-    match (func, err) {
-        (Value::Function(func), Value::Nil) => func.call::<_>(()),
-        (_, Value::String(err)) => Err(mlua::Error::runtime(format!(
-            "Error when running file {}: {}",
-            name,
-            err.to_str()?
-        ))),
-        _ => Err(mlua::Error::runtime(format!(
-            "Error when running file {}",
-            name
-        ))),
-    }
+    let function = match (func, err) {
+        (Value::Function(func), Value::Nil) => Ok(func),
+        (_, Value::String(err)) => Err(mlua::Error::runtime(err.to_string_lossy())),
+        _ => Err(mlua::Error::runtime("Unknown error")),
+    };
+
+    function
+        .and_then(|function| function.call(()))
+        .map_err(|err| err.with_context(|_| format!("Running '{}'", name)))
 }
 
 pub fn proto_to_lua_value(lua: &Lua, field: Field) -> mlua::Result<Value> {
