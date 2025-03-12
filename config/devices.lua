de6fafae7cf0d689d5de553a16bee0e1cfcf877c
llMBQll <m.balabanow@gmail.com>
2025-03-12 17:57:14 +0100

    Add official support for SteelSeries Apex Pro TKL Gen 3
    This presumably also supports other devices that switched from initial
    BitPerPixel layout to BitPerPixelVertical layout

--- config/devices.lua
+++ config/devices.lua
@@ -48,6 +48,30 @@ usb_device {
     memory_representation = 'BitPerPixel',
 }
 
+usb_device {
+    name = 'SteelSeries Apex Pro TKL Wireless Gen 3',
+    screen_size = {
+        width = 128,
+        height = 40,
+    },
+    usb_settings = {
+        vendor_id = '0x1038',
+        product_id = '0x1646',
+        interface = '0x03',
+        alternate_setting = '0x00',
+        request_type = '0x21',
+        request = '0x09',
+        value = '0x0300',
+        index = '0x03',
+    },
+    transform = function(buffer)
+        local bytes = buffer:bytes()
+        table.insert(bytes, 1, 0x0a)
+        return bytes
+    end,
+    memory_representation = 'BitPerPixelVertical',
+}
+
 emulator {
     name = 'Emulator',
     screen_size = {
