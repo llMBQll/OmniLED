2a2a7bfab80b337bc345d6b2a2dd3f0c2d8d7671
llMBQll <m.balabanow@gmail.com>
2025-11-19 16:06:28 +0100

    Remove license headers from source files
    Get rid of the bloat from wvery source file

--- omni-led/src/common/user_data.rs
+++ omni-led/src/common/user_data.rs
@@ -1,21 +1,3 @@
-/*
- * OmniLED is a software for displaying data on various OLED devices.
- * Copyright (C) 2024  Michał Bałabanow <m.balabanow@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 use mlua::{AnyUserData, IntoLua, Lua, UserData};
 use std::marker::PhantomData;
 
