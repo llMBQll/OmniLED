2d1228451a8fd480d1046734b6aa1c4a05d1250f
llMBQll <m.balabanow@gmail.com>
2026-06-10 19:50:01 +0200

    Rename EVENTS -> Events, and convert its methods to functions

--- omni-led-lib/src/events/events.rs
+++ omni-led-lib/src/events/events.rs
@@ -1,15 +1,19 @@
 use log::error;
 use mlua::{Function, Lua, UserData, UserDataMethods, Value};
 use omni_led_api::plugin::Plugin;
-use omni_led_derive::UniqueUserData;
 
 use crate::common::user_data::UniqueUserData;
 use crate::events::event_handle::EventHandle;
 use crate::events::event_queue::{Event, EventQueue};
 use crate::script_handler::script_data_types::EventKey;
 
-#[derive(UniqueUserData)]
 pub struct Events;
+
+impl UniqueUserData for Events {
+    fn identifier() -> &'static str {
+        "Events"
+    }
+}
 
 impl Events {
     pub fn load(lua: &Lua) {
@@ -53,19 +57,16 @@ impl Events {
 
 impl UserData for Events {
     fn add_methods<'lua, M: UserDataMethods<Self>>(methods: &mut M) {
-        methods.add_method(
-            "register",
-            |_lua, _this, (key, on_match): (EventKey, Function)| {
-                // Registering from user scripts must never be persistent to avoid issues on reloads
-                Ok(Self::register(key, on_match, false))
-            },
-        );
+        methods.add_function("register", |_lua, (key, on_match): (EventKey, Function)| {
+            // Registering from user scripts must never be persistent to avoid issues on reloads
+            Ok(Self::register(key, on_match, false))
+        });
 
-        methods.add_method("unregister", |_lua, _this, handle: EventHandle| {
+        methods.add_function("unregister", |_lua, handle: EventHandle| {
             Ok(Self::unregister(handle))
         });
 
-        methods.add_method("send", |_lua, _this, (event, value): (String, Value)| {
+        methods.add_function("send", |_lua, (event, value): (String, Value)| {
             Ok(Self::send(event, value))
         });
     }
