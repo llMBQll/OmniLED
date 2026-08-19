ec73a941386ebbccd40b13b3c3e194139468c0a4
llMBQll <m.balabanow@gmail.com>
2026-08-19 20:24:58 +0200

    Add steelseries api settings

--- config/settings.lua
+++ config/settings.lua
@@ -5,5 +5,10 @@ Settings {
     log_level = LevelFilter.Debug,
     keyboard_ticks_repeat_delay = 2,
     keyboard_ticks_repeat_rate = 2,
+    steelseries_api = {
+        enabled = PLATFORM.Os == 'windows',
+        register_heartbeat = true,
+        deinitialize_timeout = Duration.from_secs(15),
+    },
     update_interval = Duration.from_millis(100),
 }
