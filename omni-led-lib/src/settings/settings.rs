85bcf78d335322ab5c005c333ed4125671d0ae3b
llMBQll <m.balabanow@gmail.com>
2026-08-24 01:31:17 +0200

    Send event on settings change

--- omni-led-lib/src/settings/settings.rs
+++ omni-led-lib/src/settings/settings.rs
@@ -1,16 +1,18 @@
 use log::debug;
-use mlua::{Lua, UserData, chunk};
+use mlua::{IntoLua, Lua, UserData, UserDataFields, chunk};
 use std::time::Duration;
 
 use crate::common::lua_traits::LuaName;
 use crate::common::user_data::{UserDataRef, set_unique_user_data};
 use crate::constants::config::{ConfigType, load_config};
 use crate::create_table_with_defaults;
+use crate::events::event_queue::{Event, EventQueue};
+use crate::events::events::ScriptEvent;
 use crate::logging::logger::{LevelFilter, Log};
 use crate::renderer::font_selector::FontSelector;
 use crate::script_handler::script_data_types::DurationWrapper;
 
-#[derive(Debug, Clone, UserData)]
+#[derive(Debug, Clone)]
 pub struct Settings {
     pub animation_ticks_delay: usize,
     pub animation_ticks_rate: usize,
@@ -18,7 +20,6 @@ pub struct Settings {
     pub log_level: LevelFilter,
     pub keyboard_ticks_repeat_delay: usize,
     pub keyboard_ticks_repeat_rate: usize,
-    #[lua(skip)]
     pub update_interval: Duration,
 }
 
@@ -55,19 +56,48 @@ impl Default for Settings {
     }
 }
 
-#[mlua::userdata_impl]
-impl Settings {
-    #[lua(getter, name = "update_interval", infallible)]
-    fn get_update_interval(&self) -> DurationWrapper {
-        DurationWrapper(self.update_interval)
-    }
-
-    #[lua(setter, name = "update_interval", infallible)]
-    fn set_update_interval(&mut self, update_interval: DurationWrapper) {
-        self.update_interval = update_interval.0;
-    }
-}
-
 impl LuaName for Settings {
     const NAME: &str = "Settings";
+}
+
+macro_rules! get_set_impl {
+    ($fields:ident, $name:ident) => {
+        $fields.add_field_method_get(stringify!($name), |_, this| Ok(this.$name.clone()));
+        $fields.add_field_method_set(stringify!($name), |lua, this, val| {
+            this.$name = val;
+            EventQueue::instance()
+                .lock()
+                .unwrap()
+                .push(Event::Script(ScriptEvent {
+                    event: format!("Settings.{}", stringify!($name)),
+                    value: this.$name.clone().into_lua(&lua)?,
+                }));
+            Ok(())
+        });
+    };
+}
+
+impl UserData for Settings {
+    fn add_fields<F: UserDataFields<Self>>(fields: &mut F) {
+        get_set_impl!(fields, animation_ticks_delay);
+        get_set_impl!(fields, animation_ticks_rate);
+        get_set_impl!(fields, font);
+        get_set_impl!(fields, log_level);
+        get_set_impl!(fields, keyboard_ticks_repeat_delay);
+        get_set_impl!(fields, keyboard_ticks_repeat_rate);
+        fields.add_field_method_get("update_interval", |_, this| {
+            Ok(DurationWrapper(this.update_interval))
+        });
+        fields.add_field_method_set("update_interval", |lua, this, val: DurationWrapper| {
+            this.update_interval = val.0;
+            EventQueue::instance()
+                .lock()
+                .unwrap()
+                .push(Event::Script(ScriptEvent {
+                    event: "Settings.update_interval".to_string(),
+                    value: val.into_lua(&lua)?,
+                }));
+            Ok(())
+        });
+    }
 }
